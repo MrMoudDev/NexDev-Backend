09ff9d4afbf3af814e34bda7686d5c91fb75886f
Cristhiangar <alejandrocrsthian823@gmail.com>
2025-05-29 19:51:01 -0500

    define la ruta post con la barra de register

--- src/routes/auth.route.mjs
+++ src/routes/auth.route.mjs
@@ -1,5 +1,8 @@
 import express from "express";
+import { createUser } from "../controller/user.controller.mjs";
 
 const router = express.Router()
+
+router.post("/api/register", createUser)
 
 export default router;
